0faf128a045b463c56ae1f999fd23de14e0df6af
marcos8370 <marcos.pires@4mti.com.br>
2022-08-08 20:25:58 -0300

    add: New scopes authorization

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -47,7 +47,28 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email playlist-read-private';
+  //var scope = 'user-read-private user-read-email playlist-read-private';
+  var scope = [
+    'ugc-image-upload',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'streaming',
+    'app-remote-control',
+    'user-read-email',
+    'user-read-private',
+    'playlist-read-collaborative',
+    'playlist-modify-public',
+    'playlist-read-private',
+    'playlist-modify-private',
+    'user-library-modify',
+    'user-library-read',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-recently-played',
+    'user-follow-read',
+    'user-follow-modify'
+  ];
   const params = new URLSearchParams([
     ['response_type', 'code'],
     ['client_id', client_id],
